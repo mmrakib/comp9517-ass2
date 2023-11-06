df367a702cff850949ce6acb489ce29c38ed621e
Mayaz Rakib <mmrakib@Mayazs-MacBook-Air.local>
2023-11-06 13:33:30 +1100

    Fixed ELPVData class and added test.py file

--- src/elpvdata.py
+++ src/elpvdata.py
@@ -3,22 +3,48 @@ import sklearn.model_selection
 import random
 
 class ELPVData:
-    def __init__(test_size):
+    _instance = None
+
+    # Singleton, so we don't reload data
+    def __new__(cls):
+        if (cls.instance is None):
+            cls._instance = super(Singleton, cls).__new__(cls)
+        return cls._instance
+
+    def __init__():
         images, probs, types = elpv_reader.load_dataset()
+        x_train, x_test, y_train, y_test, probs_train, probs_test = sklearn.model_selection.train_test_split(
+            images, types, probs, test_size=0.25, random_state=random.randint(0, 100))
 
-        self.all_images = images
-        self.all_probs = probs
-        self.all_labels = types
-
-        x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(images, types, test_size, random.randint(0, 100))
+        self.images = images
+        self.probs = probs
+        self.labels = types
 
         self.training_images = x_train
         self.training_labels = y_train
+        self.training_probs = probs_train
+
         self.testing_images = x_test
         self.testing_labels = y_test
+        self.testing_probs = probs_test
 
-    def get_training_data():
-        return {data: self.training_images, labels: self.training_labels}
+    def get_all_data(self):
+        return {
+            "images": self.images,
+            "probs": self.probs,
+            "labels": self.labels
+        }
 
-    def get_training_labels():
-        return {data: self.testing_images, labels: self.testing_labels}
+    def get_training_data(self):
+        return {
+            "training_images": self.training_images,
+            "training_labels": self.training_labels,
+            "training_probs": self.training_probs
+        }
+
+    def get_testing_data(self):
+        return {
+            "testing_images": self.testing_images,
+            "testing_labels": self.testing_labels,
+            "testing_probs": self.testing_probs
+        }
---
+++ src/test.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2 as cv
+
+from elpvdata import ELPVData
+
+class Test:
+    def __init__():
+        self.data = ELPVData()
+        
