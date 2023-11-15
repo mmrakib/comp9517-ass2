dc0abfc0dbfe62e407cd0f1b2c1e95fd09bd62cc
roterrex <joel.roterex@gmail.com>
2023-11-15 16:22:29 +1100

    final? version of performance test

--- src/performancetest.py
+++ src/performancetest.py
@@ -7,8 +7,8 @@ def display_results(y_true_m, y_true_p, y_predict_m, y_predict_p):
     y_true    = np.concatenate((y_true_m,    y_true_p),    axis=0) 
     y_predict = np.concatenate((y_predict_m, y_predict_p), axis=0) 
 
-    print_scores(y_true_m, y_predict_m, "Mono Results")
-    print_scores(y_true_p, y_predict_p, "Poly Results")
+    print_scores(y_true_m, y_predict_m, "Mono Results    ")
+    print_scores(y_true_p, y_predict_p, "Poly Results    ")
     print_scores(y_true,   y_predict, "Combined Results")
     
     display_conf_mat([y_true_m, y_true, y_true_p], [y_predict_m, y_predict, y_predict_p])
@@ -37,7 +37,7 @@ def display_conf_mat(y_true_lst, y_predict_lst):
     plt_names = ["Mono", "Combined", "Poly"]
     for i in range(0,3):
         cf_matrix = sk_met.confusion_matrix(y_true_lst[i], y_predict_lst[i])
-        disp = sk_met.ConfusionMatrixDisplay(cf_matrix, display_labels=prob_name_lst[i])
+        disp = sk_met.ConfusionMatrixDisplay(cf_matrix, display_labels=prob_name_lst)
         disp.plot(ax=axes[i], xticks_rotation=45)
         disp.ax_.set_title(plt_names[i])
         disp.im_.colorbar.remove()
@@ -49,7 +49,7 @@ def display_conf_mat(y_true_lst, y_predict_lst):
     plt.subplots_adjust(wspace=0.40, hspace=0.1)
 
 
-    f.colorbar(disp.im_, ax=axes)
+    #f.colorbar(disp.im_, ax=axes)
     plt.show()
 
 def plot_train_data(history):
